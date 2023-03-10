9304d0ca5e52bdbdea4607d4a4a43825f6f52e8f
lapanikeugene <eugeny.lp@gmail.com>
2023-03-10 01:37:10 +0100

    pagination

--- text-to-anki/src/Pages/AddedText/AddedText.tsx
+++ text-to-anki/src/Pages/AddedText/AddedText.tsx
@@ -1,9 +1,11 @@
+import { Pagination } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDBConnection } from "../../hooks/dbConnection";
 import { routsLinks } from "../../routes/routsLinks";
 import { useTextStore } from "../states/textStore";
 import Word from "./Components/Word";
+import { paragraphsPerPage } from "./Components/WordSettings";
 import { AddedTextStore } from "./Store/AddedTextStore";
 
 
@@ -26,11 +28,18 @@ const AddedText = ()=>{
         // setTextForRender(""); 
         //remove all extra \n
       setTextForRender(curText.replaceAll(/\n\s*\n/g, '\n'));
+
+      
     },[])
 
+    const [paragraph,setParagraph]=useState(1);
+    const handlePage=(e:React.ChangeEvent<unknown>,p:number)=>{
+        setParagraph(p);
+    }
     useEffect(()=>{
         console.log(getTextVersion);
         setVer(getTextVersion);
+        console.log(Math.ceil(textForRender.split(/\r?\n/).length/4));
         // setTextForRender(""); 
     },[getTextVersion])
 
@@ -45,18 +54,35 @@ const AddedText = ()=>{
         <button onClick={handleAlltexts}>All texts</button>
     </div>
     <div>
-        {textForRender.split(/\r?\n/).map((a,i)=>{
+        {(paragraph-1)*paragraphsPerPage} {paragraph*paragraphsPerPage} {textForRender.split(/\r?\n/).length}
+        {textForRender.split(/\r?\n/)
+        .filter((a,i)=> (paragraph==0? i>=0 &&i<=(paragraphsPerPage-1)  
+                        :   i>(paragraph-1)*paragraphsPerPage)&&
+                            i<=(paragraph)*paragraphsPerPage)
+        
+        .map((a,i)=>{
             return (
                 <div key={`paragraph-${i}`} className={"text-left my-3 leading-8"}>
-                {a.split(" ").map((word,word_index)=>{
-                    return (<>
+                  {i}. 
+                {a.split(" ")
+             
+                
+                
+                .map((word,word_index)=>{
+                    return (<>{
+                    
+                      <>
                         <Word word={word} key={`word-${word_index}-ver-${ver}`} />{" "}
+                        </>}
                     </>)
                 })}
                 </div>
             )
         })}
       
+    </div>
+    <div>
+    <Pagination count={Math.ceil(textForRender.trim().split(/\r?\n/).length/paragraphsPerPage)} variant="outlined" onChange={handlePage} page={paragraph} />
     </div>
 
     <div>
--- text-to-anki/src/Pages/AddedText/Components/WordSettings.ts
+++ text-to-anki/src/Pages/AddedText/Components/WordSettings.ts
@@ -1,2 +1,3 @@
 export const defaultWordColor ='bg-slate-500';
 export const levelsOfWordColors=['bg-slate-400','bg-slate-300','bg-slate-200','bg-slate-100',""];
+export const paragraphsPerPage=5; 
